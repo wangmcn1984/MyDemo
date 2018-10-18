21c3e059d346e58c1dbe5ed14aa01f9778375e4a
wangmcn1984 <85135506@qq.com>
2018-10-18 19:04:52 +0800

    update

--- run_all.py
+++ run_all.py
@@ -2,13 +2,16 @@
 # -*- coding: utf-8 -*-
 """
 执行用例
+
+命令行/终端运行 Python run_all.py >> report/log.txt 2>&1
 """
 import unittest, time
 import os
-from plugins import HTMLTestRunner
+import shutil
+from plugins import HTMLTestReportCN
 
-# 或者将HTMLTestRunner文件拷贝到Python安装目录里的Lib文件夹下
-# import HTMLTestRunner
+# 或者将HTMLTestReportCN文件拷贝到Python安装目录里的Lib文件夹下
+# import HTMLTestReportCN
 
 # 获取路径
 cur_path = os.path.dirname(os.path.realpath(__file__))
@@ -22,11 +25,19 @@ if __name__ == "__main__":
     suite = unittest.defaultTestLoader.discover(case_path,'test*.py')
     # 获取当前时间
     now = time.strftime('%Y-%m-%d %H_%M_%S')
+
+    filename = report_path + '/Run_HTMLReport.html'
+    filename2 = report_path + '/' + now + ' HTMLReport.html'
+    filename3 = report_path + '/HTMLReport.html'
+
     # 定义测试报告
-    runner = HTMLTestRunner.HTMLTestRunner(title='自动化测试报告',
-                                          description='用例执行情况：',
-                                          stream=open(report_path + '\\' + now + ' HTMLReport.html', 'wb'),
-                                          verbosity=2
-                                          )
+    runner = HTMLTestReportCN.HTMLTestRunner(title='自动化测试报告',
+                                             description='用例执行情况：',
+                                             stream=open(filename, 'wb'),
+                                             verbosity=2
+                                            )
     # 运行测试用例
     runner.run(suite)
+    # 拷贝文件
+    shutil.copyfile(filename, filename2)
+    shutil.copyfile(filename, filename3)
